9bbb331a1635e20190abc7cdb5a793ed977f6743
yaras <yaras6@gmail.com>
2016-06-21 21:22:30 +0200

    Change version to 0.6.1

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -22,7 +22,7 @@ use Cake\Log\Log;
 use Cake\Auth\DefaultPasswordHasher;
 use Cake\Utility\Security;
 
-define("APP_VERSION", "0.6.0");
+define("APP_VERSION", "0.6.1");
 
 /**
  * Application Controller
